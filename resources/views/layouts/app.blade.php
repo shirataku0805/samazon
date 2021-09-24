b616cfcb57ef84c1b2bf7e5da58fbd803e4fca12
shirataku0805 <shirataku0805@gmail.com>
2021-09-24 05:50:34 +0000

    abeshira

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Samazon') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/427f4f6cf5.js" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/samazon.css')}}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @component('components.header')
+        @endcomponent
+
+        <main class="py-4 mb-5">
+            @yield('content')
+        </main>
+
+        @component('components.footer')
+        @endcomponent
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('users/mypage', 'UserController@mypage')->name('mypage');
+Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
+Route::get('users/mypage/address/edit', 'UserController@edit_address')->name('mypage.edit_address');
+Route::put('users/mypage', 'UserController@update')->name('mypage.update');
+Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
+Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+
+Route::post('products/{product}/reviews','ReviewContoroller@store');
+
+Route::get('products/{product}/favorite','ProductController@favorite')->name('products.favorite');
+Route::resource('products','ProductController');
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
